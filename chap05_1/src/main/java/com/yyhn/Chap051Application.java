2a354b4a06071c50a2b4c58c793e93f2aa233a0c
bdqntp <1361034652@qq.com>
2019-03-05 09:55:07 +0800

    ll

---
+++ chap05_1/src/main/java/com/yyhn/Chap051Application.java
@@ -0,0 +1,13 @@
+package com.yyhn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap051Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chap051Application.class, args);
+	}
+
+}
